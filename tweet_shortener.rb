67b8afc8570028ec4758087c573e0edda71bfd16
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-18 14:27:38 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,3 +29,13 @@ def bulk_tweet_shortener(tweet)
     puts word_substituter(phrase)
   end
 end
+
+def selective_tweet_shortener(tweet)
+  tweet.split(" ").map do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)
+    elsif phrase.length <= 140
+      phrase
+    end
+  end
+end
